ba217753f1afdcf03bc5187230f1f42bc4906fe5
Michal Vavrík <michal.vavrik@br-automation.com>
2021-10-27 15:53:42 +0200

    - Tmx bug with empty tmx file fixed - Generate program development started

--- CreateAlarms.py
+++ CreateAlarms.py
@@ -6,6 +6,11 @@ import re
 import sys
 import xml.etree.ElementTree as et
 
+# What will be in configuration GUI:
+# Language selection
+# Configuration selection
+# Option to run script
+
 #####################################################################################################################################################
 # Debug mode (debug print)
 #####################################################################################################################################################
@@ -77,8 +82,9 @@ for Structure in Structures:
                 Index = KEYS.index(Key)
                 Properties.append({"Key": Key, "Value": Pair[1], "Valid": True})
             except ValueError:
-                print("Warning: Key '"+Key+"' of member 'g"+Structure[0]+Structure[1]+"Type."+Member[0]+"' is not valid.")     
-        Alarms.append({"Task": Structure[0], "Type": Structure[1], "Name": Member[0], "Properties": Properties})
+                print("Warning: Key '"+Key+"' of member 'g"+Structure[0]+Structure[1]+"Type."+Member[0]+"' is not valid.")
+        if Properties:
+            Alarms.append({"Task": Structure[0], "Type": Structure[1], "Name": Member[0], "Properties": Properties})
 
 if DEBUG: print(Alarms)
 
@@ -116,8 +122,7 @@ TmxAlarms = []
 for TmxItem in TmxRoot.findall(".//tu"):
     TmxAlarms.append(TmxItem.attrib["tuid"])
 
-if DEBUG:
-    print("Tmx alamrs: " + str(TmxAlarms))
+if DEBUG: print("Tmx alamrs: " + str(TmxAlarms))
 
 #####################################################################################################################################################
 # Get alarm names from Global.typ with unique alarm name
@@ -126,8 +131,6 @@ if DEBUG:
 TypAlarms = []
 for Alarm in Alarms:
     TypAlarms.append("g" + Alarm["Task"] + "." + Alarm["Type"] + "." + Alarm["Name"])
-# Convert list to set to get unique names
-TypAlarms = set(TypAlarms)
 
 if DEBUG: print("Typ alarms: " + str(TypAlarms))
 
@@ -154,7 +157,13 @@ TmxTree.write(TmxPath)
 TmxFile = open(TmxPath, "r")
 TmxText = ""
 for TmxLine in TmxFile:
-    if (TmxLine.find("</body>") != -1): # End found
+    if (TmxLine.find("<body />") != -1): # End found
+        TmxText += TmxLine[:TmxLine.find(" />")] + ">\n"
+        TmxLine = ""
+        for NewAlarm in NewAlarms:
+            TmxText += "\t<tu tuid=\"" + NewAlarm + "\" />\n"
+        TmxText += "</body>\n"
+    elif (TmxLine.find("</body>") != -1): # End found
         for NewAlarm in NewAlarms:
             TmxText += "\t<tu tuid=\"" + NewAlarm + "\" />\n"
     TmxText += TmxLine
@@ -165,7 +174,7 @@ TmxFile.close()
 
 #TmxTree = et.parse(TmxPath)
 #TmxRoot = TmxTree.getroot()
-#TmxBody = TmxRoot.find("//body")
+#TmxBody = TmxRoot.find(".//body")
 #print(TmxBody)
 
 #####################################################################################################################################################
@@ -202,7 +211,7 @@ MpAlarmRoot = MpAlarmTree.getroot()
 
 # Remove old configuration
 Parent = MpAlarmRoot.find(".//Group[@ID=\"mapp.AlarmX.Core.Configuration\"]")
-for Group in Parent.findall("Group"):
+for Group in Parent.findall("Group"): # TODO pokud je vložený čistý MpAlarmXCore, tak v něm není element Group a tohle nic nenajde
     print(Group.attrib)
     Parent.remove(Group)
 
@@ -211,3 +220,86 @@ for Group in Parent.findall("Group"):
 
 # Save file
 MpAlarmTree.write(MpAlarmPath)
+
+#####################################################################################################################################################
+# Update alarms program TODO psát chybu, když nenajde sekci pro autogen, přidat typy Flag
+#####################################################################################################################################################
+LANGUAGE_C = 0
+LANGUAGE_ST = 1
+Extensions = [".c", ".st"]
+ProgramLanguage = LANGUAGE_ST
+if ProgramLanguage == LANGUAGE_ST:
+    ProgramPath = os.path.join(LogicalPath, "Alarms", "Alarms" + Extensions[ProgramLanguage])
+    if not os.path.isfile(ProgramPath):
+        sys.exit("File 'Alarms" + Extensions[ProgramLanguage] + "' does not exist.")
+    else:
+        # Add new alarms
+        ProgramFile = open(ProgramPath, "r")
+        ProgramText = ""
+        ErrorLastTaskName = ""
+        ProgramErrorSetText = "\t(************************************************************ Error set ************************************************************)"
+        ProgramErrorResetText = "\n\n\t(*********************************************************** Error reset ***********************************************************)"
+        WarningLastTaskName = ""
+        ProgramWarningSetText = "\n\n\t(*********************************************************** Warning set ***********************************************************)"
+        ProgramWarningResetText = "\n\n\t(********************************************************** Warning reset **********************************************************)"
+        InfoLastTaskName = ""
+        ProgramInfoSetText = "\n\n\t(************************************************************* Info set ************************************************************)"
+        ProgramInfoResetText = "\n\n\t(************************************************************ Info reset ***********************************************************)"
+        InAutomaticSection = False
+        for ProgramLine in ProgramFile:
+            if not InAutomaticSection:
+                ProgramText += ProgramLine
+            if (ProgramLine.find("// START OF AUTOMATIC CODE GENERATION //") != -1): # Automatic generation section start
+                InAutomaticSection = True
+                for Alarm in Alarms:
+                    AlarmVariable = "g" + Alarm["Task"] + "." + Alarm["Type"] + "." + Alarm["Name"]
+                    if Alarm["Type"] == "Error":
+                        if not(ErrorLastTaskName == Alarm["Task"]):
+                            ProgramErrorSetText += "\n\n\t// Task " + Alarm["Task"]
+                            ProgramErrorResetText += "\n\n\t// Task " + Alarm["Task"]
+                        ProgramErrorSetText += """
+	IF (""" + AlarmVariable + """ >  Flag.""" + AlarmVariable + """) THEN
+		MpAlarmXSet(gAlarmXCore, '""" + AlarmVariable + """');
+	END_IF;"""
+                        ProgramErrorResetText += """
+	IF (""" + AlarmVariable + """ <  Flag.""" + AlarmVariable + """) THEN
+		MpAlarmXReset(gAlarmXCore, '""" + AlarmVariable + """');
+	END_IF;"""
+                        ErrorLastTaskName = Alarm["Task"]
+                    elif Alarm["Type"] == "Warning":
+                        if not(WarningLastTaskName == Alarm["Task"]):
+                            ProgramWarningSetText += "\n\n\t// Task " + Alarm["Task"]
+                            ProgramWarningResetText += "\n\n\t// Task " + Alarm["Task"]
+                        ProgramWarningSetText += """
+	IF (""" + AlarmVariable + """ >  Flag.""" + AlarmVariable + """) THEN
+		MpAlarmXSet(gAlarmXCore, '""" + AlarmVariable + """');
+	END_IF;"""
+                        ProgramWarningResetText += """
+	IF (""" + AlarmVariable + """ <  Flag.""" + AlarmVariable + """) THEN
+		MpAlarmXReset(gAlarmXCore, '""" + AlarmVariable + """');
+	END_IF;"""
+                        WarningLastTaskName = Alarm["Task"]
+                    elif Alarm["Type"] == "Info":
+                        if not(InfoLastTaskName == Alarm["Task"]):
+                            ProgramInfoSetText += "\n\n\t// Task " + Alarm["Task"]
+                            ProgramInfoResetText += "\n\n\t// Task " + Alarm["Task"]
+                        ProgramInfoSetText += """
+	IF (""" + AlarmVariable + """ >  Flag.""" + AlarmVariable + """) THEN
+		MpAlarmXSet(gAlarmXCore, '""" + AlarmVariable + """');
+	END_IF;"""
+                        ProgramInfoResetText += """
+	IF (""" + AlarmVariable + """ <  Flag.""" + AlarmVariable + """) THEN
+		MpAlarmXReset(gAlarmXCore, '""" + AlarmVariable + """');
+	END_IF;"""
+                        InfoLastTaskName = Alarm["Task"]
+
+                ProgramText += ProgramErrorSetText + ProgramErrorResetText + ProgramWarningSetText + ProgramWarningResetText + ProgramInfoSetText + ProgramInfoResetText
+
+            elif (ProgramLine.find("// END OF AUTOMATIC CODE GENERATION //") != -1): # Automatic generation section end
+                InAutomaticSection = False
+                ProgramText += "\n\n" + ProgramLine
+        
+        ProgramFile.close()
+        ProgramFile = open(ProgramPath,"w")
+        ProgramFile.write(ProgramText)
+        ProgramFile.close()
