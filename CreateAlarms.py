797152ecc580f57a9c3025a0e8299a55e54de099
Adam Sefranek <sefranek.adam@gmail.com>
2021-11-05 08:18:20 +0100

    Add support for nested properties in mpalarmxcore

--- CreateAlarms.py
+++ CreateAlarms.py
@@ -1,8 +1,9 @@
 #####################################################################################################################################################
 # Dependencies
 #####################################################################################################################################################
-import os, re, sys
+import os, sys
 import xml.etree.ElementTree as et
+from GetAlarms import GetAlarms, CreateTreeFromProperties
 
 # What will be in configuration GUI:
 # Language selection
@@ -12,17 +13,14 @@ import xml.etree.ElementTree as et
 #####################################################################################################################################################
 # Debug mode (debug print)
 #####################################################################################################################################################
-DEBUG = True
-
-#####################################################################################################################################################
-# List of possible properties
-#####################################################################################################################################################
-KEYS = ["Code", "Severity", "Behavior"]
+DEBUG = False
 
 #####################################################################################################################################################
 # Global functions
 #####################################################################################################################################################
 # Finds file in directory and subdirectories and returns path to first found file
+
+
 def FindFilePath(SourcePath, FileName):
     FilePath = ""
     for DirPath, DirNames, FileNames in os.walk(SourcePath):
@@ -30,17 +28,20 @@ def FindFilePath(SourcePath, FileName):
             FilePath = (os.path.join(DirPath, FileNam))
     return FilePath
 
+
 # Checks if file exists and terminates script if not
 def IsFile(FilePath):
     if not os.path.isfile(FilePath):
         sys.exit("File " + os.path.basename(FilePath) + " does not exist.")
     return True
 
+
 # Checks if directory exists and terminates script if not
 def IsDir(DirPath):
     if not os.path.isdir(DirPath):
         sys.exit("Directory " + DirPath + " does not exist.")
     return True
+
 
 #####################################################################################################################################################
 # Open Global.typ file
@@ -59,67 +60,7 @@ with open(TypPath, "r") as f:
 #####################################################################################################################################################
 # Parse data from Global.typ file
 #####################################################################################################################################################
-
-# Matches structure definition, returns three groups:
-# 1. Name of the structure
-# 2. Structure suffix (Error, Info, Warning)
-# 3. Members of the structure
-PatternStructure = r"g([a-zA-Z0-9]{1,10})(Error|Info|Warning)Type[^\n]+\n([\s\S]*?)END_STRUCT"
-
-# Matches BOOL structure members with Description[2] filled in, returns two groups:
-# 1. Name of the member
-# 2. Content of Description[2]
-PatternMember = r"([a-zA-Z0-9_]{1,32}).*?BOOL[\s;]*?\(\*.*?\*\)\s*?\(\*(.+?)\*\)\s*?(?:\(.*?)?[ ]*?\n"
-
-# Matches Key=Value pairs, returns two groups:
-# 1. Key
-# 2. Value
-PatternPair = r"([a-zA-Z0-9]+)[ ]*?=[ ]*?([a-zA-Z0-9]+)"
-
-# Alarm {
-#     Task: ""
-#     Type: ""
-#     Name: ""
-#     Properties: [
-#         {
-#           Key: ""
-#           Value: ""
-#           Valid: False/True
-#         }
-#     ]
-# }
-Alarms = []
-Structures = re.findall(PatternStructure, TypContent)
-for Structure in Structures:
-    Members = re.findall(PatternMember, Structure[2])
-    for Member in Members:
-        Pairs = re.findall(PatternPair, Member[1])
-        Properties = []
-        for Pair in Pairs:
-            try:
-                Key = Pair[0].capitalize()
-                Index = KEYS.index(Key)
-                Properties.append({"Key": Key, "Value": Pair[1], "Valid": True})
-            except ValueError:
-                print("Warning: Key '"+Key+"' of member 'g"+Structure[0]+Structure[1]+"Type."+Member[0]+"' is not valid.")
-        if Properties:
-            Alarms.append({"Task": Structure[0], "Type": Structure[1], "Name": Member[0], "Properties": Properties})
-
-if DEBUG: print(Alarms)
-
-# Example
-# for Alarm in Alarms:
-#     print(Alarm["Name"])
-#     for Property in Alarm["Properties"]:
-#         print(Property["Key"],Property["Value"])
-
-#####################################################################################################################################################
-# Debug print
-#####################################################################################################################################################
-if DEBUG:
-    print("Total properties: "+str(len(Properties)))
-    for Item in Properties:
-        print(Item)
+Alarms = GetAlarms(TypContent)
 
 #####################################################################################################################################################
 # Validity of dependencies
@@ -142,14 +83,17 @@ TmxAlarms = []
 for TmxItem in TmxRoot.findall(".//tu"):
     TmxAlarms.append(TmxItem.attrib["tuid"])
 
-if DEBUG: print("Tmx alamrs: " + str(TmxAlarms))
+if DEBUG:
+    print("Tmx alarms: " + str(TmxAlarms))
 
 # Get alarm names list from Global.typ file
 TypAlarms = []
 for Alarm in Alarms:
-    TypAlarms.append("g" + Alarm["Task"] + "." + Alarm["Type"] + "." + Alarm["Name"])
+    TypAlarms.append("g" + Alarm["Task"] + "." + Alarm["Type"] + "." +
+                     Alarm["Name"])
 
-if DEBUG: print("Typ alarms: " + str(TypAlarms))
+if DEBUG:
+    print("Typ alarms: " + str(TypAlarms))
 
 # Compare alarm names lists
 NewAlarms = list(set(TypAlarms) - set(TmxAlarms))
@@ -169,25 +113,25 @@ TmxTree.write(TmxPath)
 TmxFile = open(TmxPath, "r")
 TmxText = ""
 for TmxLine in TmxFile:
-    if (TmxLine.find("<body />") != -1): # End found
+    if (TmxLine.find("<body />") != -1):  # End found
         TmxText += TmxLine[:TmxLine.find(" />")] + ">\n"
         TmxLine = ""
         for NewAlarm in NewAlarms:
             TmxText += "\t<tu tuid=\"" + NewAlarm + "\" />\n"
         TmxText += "</body>\n"
-    elif (TmxLine.find("</body>") != -1): # End found
+    elif (TmxLine.find("</body>") != -1):  # End found
         for NewAlarm in NewAlarms:
             TmxText += "\t<tu tuid=\"" + NewAlarm + "\" />\n"
     TmxText += TmxLine
 TmxFile.close()
-TmxFile = open(TmxPath,"w")
+TmxFile = open(TmxPath, "w")
 TmxFile.write(TmxText)
 TmxFile.close()
 
 #TmxTree = et.parse(TmxPath)
 #TmxRoot = TmxTree.getroot()
 #TmxBody = TmxRoot.find(".//body")
-#print(TmxBody)
+# print(TmxBody)
 
 #####################################################################################################################################################
 # Update mpalarmxcore
@@ -202,7 +146,7 @@ if (ConfigPath.find("Logical") != -1):
         if (Physical.find("Physical") != -1):
             ConfigPath += "Physical"
             for Config in os.listdir(ConfigPath):
-                if not(Config.endswith(".pkg")):
+                if not (Config.endswith(".pkg")):
                     ConfigName.append(Config)
             break
 
@@ -223,21 +167,42 @@ MpAlarmRoot = MpAlarmTree.getroot()
 # Remove old configuration
 # Parent = MpAlarmRoot.find(".//Group[@ID=\"mapp.AlarmX.Core.Configuration\"]")
 Parent = MpAlarmRoot.find(".//Element[@Type=\"mpalarmxcore\"]")
-for Group in Parent.findall(".//Group[@ID=\"mapp.AlarmX.Core.Configuration\"]"):
+for Group in Parent.findall(
+        ".//Group[@ID=\"mapp.AlarmX.Core.Configuration\"]"):
     Parent.remove(Group)
 
 MpAlarmList = et.Element("Group", {"ID": "mapp.AlarmX.Core.Configuration"})
 
 # Insert new configuration
-for Index, Alarm in enumerate(Alarms):
-    Elem = et.Element("Group", {"ID": "["+str(Index)+"]"})
+
+
+def MpAlarmCreateNodes(Parent, Properties) -> et.Element:
+    for Item in Properties:
+        if Item.data:
+            Attrib = {"ID": Item.key}
+            if Item.data["Value"]:
+                Attrib["Value"] = Item.data["Value"]
+            Element = et.Element(Item.data["Tag"], Attrib)
+            MpAlarmCreateNodes(Element, Item)
+            Parent.append(Element)
+    return Parent
+
+
+def MpAlarmCreateGroup(Index: int, Alarm: dict) -> et.Element:
+    Group = et.Element("Group", {"ID": "["+str(Index)+"]"})
     Name = "g"+Alarm["Task"]+"."+Alarm["Type"]+"."+Alarm["Name"]
     Message = "{$User/Alarms/"+Name+"}"
-    et.SubElement(Elem, "Property", {"ID": "Name", "Value": Name})
-    et.SubElement(Elem, "Property", {"ID": "Message", "Value": Message})
-    for Property in Alarm["Properties"]:
-        et.SubElement(Elem, "Property", {"ID": Property["Key"], "Value": Property["Value"]})
-    MpAlarmList.append(Elem)
+    et.SubElement(Group, "Property", {"ID": "Name", "Value": Name})
+    et.SubElement(Group, "Property", {"ID": "Message", "Value": Message})
+    Properties = CreateTreeFromProperties(Alarm["Properties"])
+    MpAlarmCreateNodes(Group, Properties)
+    return Group
+
+
+for Index, Alarm in enumerate(Alarms):
+    Element = MpAlarmCreateGroup(Index, Alarm)
+    MpAlarmList.append(Element)
+
 Parent.append(MpAlarmList)
 
 # Save file
@@ -246,6 +211,8 @@ MpAlarmTree.write(MpAlarmPath)
 #####################################################################################################################################################
 # Update alarms program TODO přidat datové typy Flag, udělat jazyk C
 #####################################################################################################################################################
+
+
 def AlarmSetReset(VariableSetText, VariableResetText, AlarmVariable):
     VariableSetText += """
 	IF (""" + AlarmVariable + """ >  Flag.""" + AlarmVariable + """) THEN
@@ -257,12 +224,14 @@ def AlarmSetReset(VariableSetText, VariableResetText, AlarmVariable):
 	END_IF;"""
     return VariableSetText, VariableResetText
 
+
 LANGUAGE_C = 0
 LANGUAGE_ST = 1
 Extensions = [".c", ".st"]
 ProgramLanguage = LANGUAGE_ST
 if ProgramLanguage == LANGUAGE_ST:
-    ProgramPath = FindFilePath(LogicalPath, "Alarms" + Extensions[ProgramLanguage])
+    ProgramPath = FindFilePath(LogicalPath,
+                               "Alarms" + Extensions[ProgramLanguage])
     if IsFile(ProgramPath):
         # Create whole automatically generated section and insert it to the file
         ProgramFile = open(ProgramPath, "r")
@@ -282,43 +251,61 @@ if ProgramLanguage == LANGUAGE_ST:
         for ProgramLine in ProgramFile:
             if not InAutomaticSection:
                 ProgramText += ProgramLine
-            if (ProgramLine.find("// START OF AUTOMATIC CODE GENERATION //") != -1): # Automatic generation section start
+            if (ProgramLine.find("// START OF AUTOMATIC CODE GENERATION //") !=
+                    -1):  # Automatic generation section start
                 AutomaticSectionStartFound = True
                 InAutomaticSection = True
                 for Alarm in Alarms:
-                    AlarmVariable = "g" + Alarm["Task"] + "." + Alarm["Type"] + "." + Alarm["Name"]
+                    AlarmVariable = "g" + Alarm["Task"] + "." + Alarm[
+                        "Type"] + "." + Alarm["Name"]
                     if Alarm["Type"] == "Error":
-                        if not(ErrorLastTaskName == Alarm["Task"]):
-                            ProgramErrorSetText += "\n\n\t// Task " + Alarm["Task"]
-                            ProgramErrorResetText += "\n\n\t// Task " + Alarm["Task"]
-                        ProgramErrorSetText, ProgramErrorResetText = AlarmSetReset(ProgramErrorSetText, ProgramErrorResetText, AlarmVariable)
+                        if not (ErrorLastTaskName == Alarm["Task"]):
+                            ProgramErrorSetText += "\n\n\t// Task " + Alarm[
+                                "Task"]
+                            ProgramErrorResetText += "\n\n\t// Task " + Alarm[
+                                "Task"]
+                        ProgramErrorSetText, ProgramErrorResetText = AlarmSetReset(
+                            ProgramErrorSetText, ProgramErrorResetText,
+                            AlarmVariable)
                         ErrorLastTaskName = Alarm["Task"]
                     elif Alarm["Type"] == "Warning":
-                        if not(WarningLastTaskName == Alarm["Task"]):
-                            ProgramWarningSetText += "\n\n\t// Task " + Alarm["Task"]
-                            ProgramWarningResetText += "\n\n\t// Task " + Alarm["Task"]
-                        ProgramWarningSetText, ProgramWarningResetText = AlarmSetReset(ProgramWarningSetText, ProgramWarningResetText, AlarmVariable)
+                        if not (WarningLastTaskName == Alarm["Task"]):
+                            ProgramWarningSetText += "\n\n\t// Task " + Alarm[
+                                "Task"]
+                            ProgramWarningResetText += "\n\n\t// Task " + Alarm[
+                                "Task"]
+                        ProgramWarningSetText, ProgramWarningResetText = AlarmSetReset(
+                            ProgramWarningSetText, ProgramWarningResetText,
+                            AlarmVariable)
                         WarningLastTaskName = Alarm["Task"]
                     elif Alarm["Type"] == "Info":
-                        if not(InfoLastTaskName == Alarm["Task"]):
-                            ProgramInfoSetText += "\n\n\t// Task " + Alarm["Task"]
-                            ProgramInfoResetText += "\n\n\t// Task " + Alarm["Task"]
-                        ProgramInfoSetText, ProgramInfoResetText = AlarmSetReset(ProgramInfoSetText, ProgramInfoResetText, AlarmVariable)
+                        if not (InfoLastTaskName == Alarm["Task"]):
+                            ProgramInfoSetText += "\n\n\t// Task " + Alarm[
+                                "Task"]
+                            ProgramInfoResetText += "\n\n\t// Task " + Alarm[
+                                "Task"]
+                        ProgramInfoSetText, ProgramInfoResetText = AlarmSetReset(
+                            ProgramInfoSetText, ProgramInfoResetText,
+                            AlarmVariable)
                         InfoLastTaskName = Alarm["Task"]
 
-                ProgramText += ProgramErrorSetText + ProgramErrorResetText + ProgramWarningSetText + ProgramWarningResetText + ProgramInfoSetText + ProgramInfoResetText
+                ProgramText += ProgramErrorSetText + ProgramErrorResetText + ProgramWarningSetText + \
+                    ProgramWarningResetText + ProgramInfoSetText + ProgramInfoResetText
 
-            elif (ProgramLine.find("// END OF AUTOMATIC CODE GENERATION //") != -1): # Automatic generation section end
+            elif (ProgramLine.find("// END OF AUTOMATIC CODE GENERATION //") !=
+                  -1):  # Automatic generation section end
                 AutomaticSectionEndFound = True
                 InAutomaticSection = False
                 ProgramText += "\n\n" + ProgramLine
-        
+
         ProgramFile.close()
         if not AutomaticSectionStartFound:
-            sys.exit("Start of automatically generated section in Alarms" + Extensions[ProgramLanguage] + " not found.")
+            sys.exit("Start of automatically generated section in Alarms" +
+                     Extensions[ProgramLanguage] + " not found.")
         elif not AutomaticSectionEndFound:
-            sys.exit("End of automatically generated section in Alarms" + Extensions[ProgramLanguage] + " not found.")
+            sys.exit("End of automatically generated section in Alarms" +
+                     Extensions[ProgramLanguage] + " not found.")
         else:
-            ProgramFile = open(ProgramPath,"w")
+            ProgramFile = open(ProgramPath, "w")
             ProgramFile.write(ProgramText)
             ProgramFile.close()
---
+++ GetAlarms.py
@@ -0,0 +1,96 @@
+import re
+from Tree import Node
+
+# Each key represents allowed alarm property, its value is XML element tag
+PROPERTIES = {"Code": "Property", "Severity": "Property", "Behavior": "Selector",
+              "Behavior.Retain": "Property", "Behavior.Asynchronous": "Property",
+              "Behavior.Monitoring": "Group", "Behavior.Monitoring.MonitoredPV": "Property",
+              "Behavior.Monitoring.LowLimitEnable": "Selector", "Behavior.Monitoring.LowLimitEnable.Limit": "Property",
+              "Behavior.Monitoring.HighLimitEnable": "Selector", "Behavior.Monitoring.HighLimitEnable.Limit": "Property"}
+
+# Matches structure definition, returns three groups:
+# 1. Name of the structure
+# 2. Structure suffix (Error, Info, Warning)
+# 3. Members of the structure
+PATTERN_STRUCTURE = r"g([a-zA-Z0-9]{1,10})(Error|Info|Warning)Type[^\n]+\n([\s\S]*?)END_STRUCT"
+
+# Matches BOOL structure members with Description[2] filled in, returns two groups:
+# 1. Name of the member
+# 2. Content of Description[2]
+PATTERN_MEMBER = r"([a-zA-Z0-9_]{1,32}).*?BOOL[\s;]*?\(\*.*?\*\)\s*?\(\*(.+?)\*\)\s*?(?:\(.*?)?[ ]*?\n"
+
+# Matches Key=Value pairs, returns two groups:
+# 1. Key
+# 2. Value
+PATTERN_PAIR = r"([a-zA-Z0-9.]+)[ ]*?=[ ]*?([a-zA-Z0-9]+)"
+
+
+def GetAlarms(Input: str) -> list:
+    """
+    Parses Input string and returns list of Alarms.
+
+    Alarm {
+        Task: ""
+        Type: ""
+        Name: ""
+        Properties: [
+            {
+                Key: ""
+                Value: ""
+                Valid: False/True
+                Tag: ""
+            }
+        ]
+    }
+    """
+    Alarms = []
+    Structures = re.findall(PATTERN_STRUCTURE, Input)
+    for Structure in Structures:
+        Members = re.findall(PATTERN_MEMBER, Structure[2])
+        for Member in Members:
+            Pairs = re.findall(PATTERN_PAIR, Member[1])
+            Properties = []
+            for Pair in Pairs:
+                if Pair[0] in PROPERTIES:
+                    Properties.append(
+                        {"Key": Pair[0], "Value": Pair[1], "Valid": True, "Tag": PROPERTIES[Pair[0]]})
+                else:
+                    print("Warning: Key '"+Pair[0]+"' of member 'g"+Structure[0] +
+                          Structure[1]+"Type."+Member[0]+"' is not valid.")
+            if Properties:
+                Properties = sorted(Properties, key=lambda d: d["Key"])
+                Alarms.append(
+                    {"Task": Structure[0], "Type": Structure[1], "Name": Member[0], "Properties": Properties})
+    return Alarms
+
+
+class Node(object):
+    def __init__(self, key, data=None):
+        self.key = key
+        self.data = data
+        self.children = []
+
+    def __iter__(self):
+        return iter(self.children)
+
+    def append(self, obj):
+        self.children.append(obj)
+
+    def find(self, key):
+        return next(iter([node for node in self.children if node.key == key]), None)
+
+
+def CreateTreeFromProperties(Properties: list) -> Node:
+    Tree = Node("Root")
+    for Item in Properties:
+        Keys = Item["Key"].split(".")
+        Last = Keys.pop(-1)
+        Parent = Tree
+        for Index, Key in enumerate(Keys):
+            Child = Parent.find(Key)
+            if Child:
+                Parent = Child
+            else:
+                Parent.append(Node(Key))
+        Parent.append(Node(Last, Item))
+    return Tree
